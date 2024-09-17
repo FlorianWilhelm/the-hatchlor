f1bb8529d1e348e009731e1369b6ff6b048afc4c
turboBasic ⚡️💀 <andrii7melnyk@proton.me>
2024-09-17 22:48:37 +0200

    Add empty root commit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,11 @@
 import subprocess
 
 try:
-    subprocess.call(['git', 'init'])
+    subprocess.call(['git', 'init', '--initial-branch', 'main'])
+    subprocess.call(['git', 'commit', '--allow-empty', '-m', 'Root commit'])
     subprocess.call(['git', 'add', '*'])
-    subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+    subprocess.call(['git', 'commit', '-m', 'Initial commit from Cookiecutter template'])
     subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.project_repo }}'])
 except Exception as e:
     print(f"An error occurred during initializing the git repo: {e}")
-    print("Makre sure to manually set up a git repository which is necessary for `hatch-vcs`")
+    print("Make sure to manually set up a git repository which is necessary for `hatch-vcs`")
